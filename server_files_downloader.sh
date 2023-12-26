b8333a92658b5cfd31458639fe5e3c9cc2b14a51
Francesco Milano <milano.francesco@protonmail.com>
2023-12-26 21:14:42 +0100

    Added steamcmd to sh

--- server_files_downloader.sh
+++ server_files_downloader.sh
@@ -1,13 +1,15 @@
 SteamcmdUrl="https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip"
 BoiiiUrl="https://github.com/Ezz-lol/boiii-free/releases/latest/download/boiii.exe"
 
+sudo add-apt-repository multiverse; sudo dpkg --add-architecture i386; sudo apt update
+sudo apt install steamcmd
+
 #Downloading server files
 steamcmd +force_install_dir .. +login anonymous +app_update 545990 validate +quit
 
 #deleting unecessary folders
 cd ..
 rm -rf steamapps
-rm -rf steamcmd
 
 #deleting unecessary files
 cd UnrankedServer
