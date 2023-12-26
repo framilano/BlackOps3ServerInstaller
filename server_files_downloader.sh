6eeee0879791a56dff85de8fb4d47bd3da2fc6d8
Francesco Milano <milano.francesco@protonmail.com>
2023-12-26 19:34:32 +0100

    Added draft for sh

---
+++ server_files_downloader.sh
@@ -0,0 +1,25 @@
+SteamcmdUrl="https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip"
+BoiiiUrl="https://github.com/Ezz-lol/boiii-free/releases/latest/download/boiii.exe"
+
+#Retrieves SteamCMD
+curl -s %SteamcmdUrl -o steamcmd.zip
+mkdir steamcmd
+mv steamcmd.zip steamcmd
+cd steamcmd
+tar -xf steamcmd.zip
+
+#Downloading server files
+steamcmd +force_install_dir .. +login anonymous +app_update 545990 validate +quit
+
+#deleting unecessary folders
+cd ..
+rm -rf steamapps
+rm -rf steamcmd
+
+#deleting unecessary files
+cd UnrankedServer
+#del copydedicated.bat
+#del Launch_Server.bat
+
+#downloading latest t7/boiii/bo3 client
+curl -L $BoiiiUrl -o boiii.exe
