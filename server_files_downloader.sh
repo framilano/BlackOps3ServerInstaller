4460393dd31fefbf3a3343cda12e38cc738f0b6b
Francesco Milano <milano.francesco@protonmail.com>
2025-05-09 00:56:56 +0200

    Added arch draft

--- server_files_downloader.sh
+++ server_files_downloader.sh
@@ -1,35 +1,52 @@
 T7xUrl="https://master.bo3.eu/t7x/t7x.exe"
 
-#Updating stuff
-sudo apt update
-sudo apt upgrade -y
-
-#Install curl and wget
-sudo apt install curl wget -y
-
-#WINE Stuff
-sudo dpkg --add-architecture i386
-sudo mkdir -pm755 /etc/apt/keyrings
-
-if [ $1 == "ubuntu" ]; then
-    sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
-    sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/noble/winehq-noble.sources
+if [ $1 == "ubuntu" ] || [ $1 == "debian" ]; then
+    #Updating stuff
     sudo apt update
-    sudo apt install --install-recommends winehq-staging -y
-    #Retrieves SteamCMD
-    sudo add-apt-repository multiverse -y; sudo apt update
-    sudo apt install steamcmd -y
+    sudo apt upgrade -y
+
+    #Install curl and wget
+    sudo apt install curl wget -y
+
+    #WINE Stuff
+    sudo dpkg --add-architecture i386
+    sudo mkdir -pm755 /etc/apt/keyrings
+
+    if [ $1 == "ubuntu" ]; then
+        sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+        sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/noble/winehq-noble.sources
+        sudo apt update
+        sudo apt install --install-recommends winehq-staging -y
+        #Retrieves SteamCMD
+        sudo add-apt-repository multiverse -y; sudo apt update
+        sudo apt install steamcmd -y
+    fi
+
+    if [ $1 == "debian" ]; then
+        wget -O - https://dl.winehq.org/wine-builds/winehq.key | sudo gpg --dearmor -o /etc/apt/keyrings/winehq-archive.key -
+        sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bookworm/winehq-bookworm.sources
+        sudo apt update
+        sudo apt install --install-recommends winehq-staging -y
+        #Retrieves SteamCMD
+        sudo apt install software-properties-common -y
+        sudo apt-add-repository non-free -y
+        sudo apt install steamcmd -y
+    fi
 fi
 
-if [ $1 == "debian" ]; then
-    wget -O - https://dl.winehq.org/wine-builds/winehq.key | sudo gpg --dearmor -o /etc/apt/keyrings/winehq-archive.key -
-    sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bookworm/winehq-bookworm.sources
-    sudo apt update
-    sudo apt install --install-recommends winehq-staging -y
-    #Retrieves SteamCMD
-    sudo apt install software-properties-common -y
-    sudo apt-add-repository non-free -y
-    sudo apt install steamcmd -y
+if [ $1 == "arch" ]; then
+    #Updating stuff
+    sudo pacman -Syu
+
+    #Install curl and wget
+    sudo pacman -Sy curl wget base-devel
+
+    #WINE Stuff
+    sudo pacman -Sy wine-staging
+    #Retrieves SteamCMD and build SteamCMD
+    git clone https://aur.archlinux.org/steamcmd.git
+    cd steamcmd
+    makepkg -si
 fi
 
 
